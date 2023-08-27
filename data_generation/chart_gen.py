is_inside_bbox - bug fix (wrong dim use)
set_ax_loc_rotate - changing noise

data_generation/chart_gen.py
@@ -13,6 +13,7 @@ import matplotlib.transforms as mtransforms
 def generate_line_chart(x, y, line_color='blue', grid_style="both", x_title=None, y_title=None, graph_title=None,
                         theme="default",
                         line_style='-', marker_style=None, figsize=(6, 4), name=None, rotate=False, show=True):
+    # TODO add option for random data points addition (just for plot not sieries) for numeric  data
     set_style(theme)
     fig, ax = plt.subplots(figsize=figsize)
     set_grid(grid_style, ax)
@@ -437,12 +438,12 @@ def generate_n_plots(data_series, generated_imgs, n=2, data_types=["line", "scat
 
 
 if __name__ == "__main__":
-    data_series_path = r"D:\MGA\data_series.csv"
-    data_series = preprocess_data_series(pd.read_csv(data_series_path))
+    # data_series_path = r"D:\MGA\data_series.csv"
+    # data_series = preprocess_data_series(pd.read_csv(data_series_path))
     generated_imgs = r"D:\MGA\gen_line"
-    data_types = ["line"]# ["line", "scat", "dot", "bar"]
-    data_list = generate_n_plots(data_series, generated_imgs, n=10000, data_types=data_types,
-                                 show=False, clear_list=True)
+    # data_types = ["line"]# ["line", "scat", "dot", "bar"]
+    # data_list = generate_n_plots(data_series, generated_imgs, n=10000, data_types=data_types,
+    #                              show=False, clear_list=True)
     # df = pd.DataFrame.from_records(data_list)
     # df.to_csv(os.path.join(generated_imgs, "generated_data.csv"))
     #
@@ -454,3 +455,10 @@ if __name__ == "__main__":
     # img_name = os.path.join(generated_imgs, f"{final_name}.jpg")
     # boxes = get_bboxes(data_dict)
     # plot_image_with_boxes(img_name, boxes, jupyter=False)
+
+    data_dict, final_name = random_generate_line_chart(np.linspace(0, 100, 10).astype(int), np.sin(np.linspace(0, 10, 10)),
+                                                       name=os.path.join(generated_imgs, "line"),
+                                                       x_title="x_title", y_title="y_title", graph_title="graph_title")
+    img_name = os.path.join(generated_imgs, f"{final_name}.jpg")
+    boxes = get_bboxes(data_dict, gen=True)
+    plot_image_with_boxes(img_name, boxes, jupyter=False)

data_generation/gen_ax_fig.py
@@ -29,12 +29,14 @@ def filter_texts(texts, plot_bbox, pixel_dims):
         """Check if a polygon is inside a bounding box."""
         if not is_numeric(text):
             return True
-        return (((polygon['x0']+polygon['x1'])/2 <= bbox.x1*pixel_dims[1] and (polygon['x0']+polygon['x1'])/2 >= bbox.x0*pixel_dims[1]-4) or
-                ((polygon['y2']+polygon['y0'])/2 <= bbox.y1*pixel_dims[0]+8 and (polygon['y2']+polygon['y0'])/2 >= bbox.y0*pixel_dims[0]+1))
+        return (((polygon['x0']+polygon['x1'])/2 <= bbox.x1*pixel_dims[0] and
+                 (polygon['x0']+polygon['x1'])/2 >= bbox.x0*pixel_dims[0]) or
+                (polygon['y0'] <= bbox.y1*pixel_dims[1] and
+                 polygon['y2'] >= bbox.y0*pixel_dims[1]))
     return [text for text in texts if is_inside_bbox(text['polygon'], plot_bbox, pixel_dims, text['text'])]
 
 
-def bbox_to_polygon(bbox, pixel_dims, fig_inch_size, shift = 1):
+def bbox_to_polygon(bbox, pixel_dims, fig_inch_size, shift=1):
     return {
         'x0': bbox.x0*pixel_dims[0]-shift,
         'x1': bbox.x1*pixel_dims[0]+shift,
@@ -179,11 +181,11 @@ def set_x_ticks(ax, x, rotate):
 
 
 def set_ax_loc_rotate(ax, rotate):
-    rand_noise = np.random.uniform(0.1, 0.15)
-    ax.set_position([rand_noise, rand_noise, 1 - rand_noise * 2, 1 - rand_noise * 2])
+    rand_noise_x = np.random.uniform(0.1, 0.125)
+    rand_noise_y = np.random.uniform(0.075, 0.1)
+    ax.set_position([rand_noise_x, rand_noise_y, 1 - rand_noise_x*1.25, 1 - rand_noise_y * 2])
     if rotate:
-        ax.margins(0.05)
-        ax.set_position([0.075 + rand_noise, 0.075 + rand_noise, 0.85 - rand_noise * 2, 0.85 - rand_noise * 2])
+        ax.set_position([0.075 + rand_noise_x, 0.075 + rand_noise_y, 0.85 - rand_noise_x * 2, 0.85 - rand_noise_y * 2])
 
 
 def default_serialize(obj):